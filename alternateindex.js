80ca5735dda06b915b302f7957f45032b20430db
SaintDrew <drew_el12@yahoo.com>
2024-06-05 04:08:19 -0400

    Adding interceptors

--- alternateindex.js
+++ alternateindex.js
@@ -7,6 +7,15 @@ const infoDump = document.getElementById("infoDump");
 const inputEl = document.getElementById("breedSelect").value;
 const url = `https://api.thecatapi.com/v1/breeds`
 let inputData = "";
+/**
+* 4. Change all of your fetch() functions to axios!
+* - axios has already been imported for you within index.js.
+* - If you've done everything correctly up to this point, this should be simple.
+* - If it is not simple, take a moment to re-evaluate your original code.
+* - Hint: Axios has the ability to set default headers. Use this to your advantage
+*   by setting a default header with your API key so that you do not have to
+*   send it manually with all of your requests! You can also set a default base URL!
+*/
 
 axios.get(url)
 .then(reponse => {
@@ -67,4 +76,20 @@ breedSelect.addEventListener('change', async function(event) {
     } catch (error) {
         console.error('Error fetching breed info:' , error);
     }
+});
+
+/**
+ * 5. Add axios interceptors to log the time between request and response to the console.
+ * - Hint: you already have access to code that does this!
+ * - Add a console.log statement to indicate when requests begin.
+ * - As an added challenge, try to do this on your own without referencing the lesson material.
+ */
+
+// Adding a request interceptor 
+axios.interceptors.request.use(function(config){
+    // Logging whent he reuqest happens
+    console.log('Request initiated at:' , new Date().toLocaleDateString());
+return config;
+}, function(error){
+    return Promise.reject(error);
 });
